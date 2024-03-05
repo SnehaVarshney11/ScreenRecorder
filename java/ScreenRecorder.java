9e6944b0541896b605caa4db14d5c4abf8226062
sneha <sneha.varshney_cs19@gla.ac.in>
2024-03-05 17:49:37 +0530

    Added start/stop button

--- java/ScreenRecorder.java
+++ java/ScreenRecorder.java
@@ -1,46 +1,109 @@
 import java.awt.AWTException;
+import java.awt.BorderLayout;
 import java.awt.Rectangle;
 import java.awt.Robot;
 import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
 import org.jcodec.api.awt.AWTSequenceEncoder;
 
-public class ScreenRecorder {
-    public static void main(String[] args) throws AWTException, IOException {
-        List<BufferedImage> imgList = new ArrayList<>();
+public class ScreenRecorder extends JFrame{
 
-        Rectangle screeRectangle = new Rectangle(Toolkit.getDefaultToolkit().getScreenSize());
-        Robot robot = new Robot();
+    private final JButton startButton;
+    private final JButton stopButton;
+    private final JLabel statusLabel;
+    private final List<BufferedImage> imgList;
+    private final Rectangle screenRectangle;
+    private final Robot robot;
+    private final File file;
+    private AWTSequenceEncoder sequenceEncoder;
+    private volatile boolean isRecording;
 
-        File file = new File("outputVideo.mp4");
+    public ScreenRecorder() throws AWTException {
+        super("Screen Recorder");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        System.out.println("Getting scan images...");
+        startButton = new JButton("Start");
+        stopButton = new JButton("Stop");
+        statusLabel = new JLabel("Status: IDLE");
+        imgList = new ArrayList<>();
+        screenRectangle = new Rectangle(Toolkit.getDefaultToolkit().getScreenSize());
+        robot = new Robot();
+        file = new File("outputVideo.mp4");
 
-        int count = 0;
+        startButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                startRecording();
+            }
+        });
 
-        while (count < 100) {
-            BufferedImage img = robot.createScreenCapture(screeRectangle);
-            imgList.add(img);
+        stopButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                stopRecording();
+            }
+        });
 
-            count++;
-        }
+        // LAYOUTS COMPONENTS
+        JPanel btnPanel = new JPanel();
+        btnPanel.add(startButton);
+        btnPanel.add(stopButton);
 
-        makeVideoFromImage(imgList, file);
+        JPanel statusPanel = new JPanel();
+        statusPanel.add(statusLabel);
+
+        setLayout(new BorderLayout());
+        add(btnPanel, BorderLayout.NORTH);
+        add(statusPanel, BorderLayout.SOUTH);
+
+        pack();
+        setLocationRelativeTo(null); //center window
+        setVisible(true);
+
     }
 
-    public static void makeVideoFromImage(List<BufferedImage> imgList, File file) throws IOException {
-        AWTSequenceEncoder sequenceEncoder = AWTSequenceEncoder.createSequenceEncoder(file, 25);
-
-        for(int i=0; i<imgList.size(); i++) {
-            sequenceEncoder.encodeImage(imgList.get(i));
-
-            System.out.println("List encode " + i);
+    private void startRecording() {
+        if (!isRecording) {
+            isRecording = true;
+            new Thread(() -> {
+                try {
+                    sequenceEncoder = AWTSequenceEncoder.createSequenceEncoder(file, 25);
+                    int count = 0;
+                    while (isRecording && count < 100) {
+                        BufferedImage img = robot.createScreenCapture(screenRectangle);
+                        imgList.add(img);
+                        sequenceEncoder.encodeImage(img);
+                        count++;
+                        statusLabel.setText("Status: Recording - Frames captured: " + count);
+                        Thread.sleep(40); 
+                    }
+                    sequenceEncoder.finish();
+                    statusLabel.setText("Status: Recording stopped");
+                    isRecording = false;
+                } catch (IOException | InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }).start();
         }
-        sequenceEncoder.finish();
     }
+
+
+    private void stopRecording() {
+        isRecording = false;
+    }
+
+    public static void main(String[] args) throws AWTException {
+        new ScreenRecorder();
+    }
+
 }
